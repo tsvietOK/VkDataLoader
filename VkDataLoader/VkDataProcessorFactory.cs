95cf41d389f2f6769707e938df7c43a4e295c44e
Keksilton <13134670+Keksilton@users.noreply.github.com>
2022-05-01 19:41:54 -0400

    Fix build.

--- VkDataLoader/VkDataProcessorFactory.cs
+++ VkDataLoader/VkDataProcessorFactory.cs
@@ -8,7 +8,7 @@ namespace VkDataLoader
     {
         public static VkDataProcessor GetVkDataProcessor(string folderPath)
         {
-
+            throw new NotImplementedException();
         }
     }
 }
